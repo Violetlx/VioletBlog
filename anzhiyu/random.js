d35f4abc4a6a3ce163baf0bdacd750d482e9f130
lixuan <2789968443@qq.com>
2024-09-18 14:23:10 +0800

    Site updated: 2024-09-18 14:23:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html","posts/4a17b156.html","posts/c68db7e6.html","posts/f5f9fa9b.html","posts/6b830637.html","posts/97a0a1c6.html","posts/8af9e237.html","posts/545a6120.html"];function toRandomPost(){
+var posts=["posts/undefined.html","posts/4a17b156.html","posts/c68db7e6.html","posts/f5f9fa9b.html","posts/97a0a1c6.html","posts/545a6120.html","posts/6b830637.html","posts/8af9e237.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
